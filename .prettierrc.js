3b481ba6e1532b912874cd904e1381d3cab2af54
oliverjam <oliverjamesphillips@gmail.com>
2019-01-26 15:17:03 +0000

    Init project

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// explicitly set all Prettier config to defaults (except trailing commas)
+// should stop contributors' personal editor configs from changing anything
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2
+  tabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5", // I'm not a savage
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+};
