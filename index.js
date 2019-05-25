31398a165a0ab94fff3679c14799b43660ff0571
oliverjam <oliverjamesphillips@gmail.com>
2019-05-25 17:18:06 +0100

    Add rules-of-hooks plugin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  parser: 'babel-eslint',
-  plugins: ['import', 'react'],
+  parser: "babel-eslint",
+  plugins: ["import", "react"],
   env: {
     browser: true,
     es6: true,
@@ -8,43 +8,46 @@ module.exports = {
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   // https://github.com/benmosher/eslint-plugin-import/blob/master/config/errors.js
-  extends: ['plugin:import/errors'],
+  extends: ["plugin:import/errors"],
   rules: {
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-comment-textnodes.md
-    'react/jsx-no-comment-textnodes': 'warn',
+    "react/jsx-no-comment-textnodes": "warn",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
-    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
+    "react/jsx-no-duplicate-props": ["warn", { ignoreCase: true }],
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md
-    'react/jsx-no-target-blank': 'warn',
+    "react/jsx-no-target-blank": "warn",
     //https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-undef.md
-    'react/jsx-no-undef': 'error',
+    "react/jsx-no-undef": "error",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
-    'react/jsx-pascal-case': [
-      'warn',
+    "react/jsx-pascal-case": [
+      "warn",
       {
         allowAllCaps: true,
         ignore: [],
       },
     ],
     //https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-react.md
-    'react/jsx-uses-react': 'warn',
+    "react/jsx-uses-react": "warn",
     //https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-vars.md
-    'react/jsx-uses-vars': 'warn',
+    "react/jsx-uses-vars": "warn",
     //https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger-with-children.md
-    'react/no-danger-with-children': 'warn',
+    "react/no-danger-with-children": "warn",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
-    'react/no-direct-mutation-state': 'warn',
+    "react/no-direct-mutation-state": "warn",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-typos.md
-    'react/no-typos': 'error',
+    "react/no-typos": "error",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
-    'react/react-in-jsx-scope': 'error',
+    "react/react-in-jsx-scope": "error",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-render-return.md
-    'react/require-render-return': 'error',
+    "react/require-render-return": "error",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/style-prop-object.md
-    'react/style-prop-object': 'warn',
+    "react/style-prop-object": "warn",
+    // https://www.npmjs.com/package/eslint-plugin-react-hooks
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
   },
 };
