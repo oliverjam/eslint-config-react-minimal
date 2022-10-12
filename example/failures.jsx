47bea1b78f0059c396e44ee374f96996bea8caea
oliverjam <oliverjamesphillips@gmail.com>
2022-10-12 14:12:08 +0100

    Enable more "guidance" rules.
    Things like "empty elements self close"
    or "you don't need a fragment with 1 child"
    are useful feedback for beginners.

--- example/failures.jsx
+++ example/failures.jsx
@@ -18,16 +18,42 @@
 // react/style-prop-object
 <p style="color: red">test</p>;
 
-// "react-hooks/rules-of-hooks"
+// react-hooks/rules-of-hooks
 function UsesHooksWrong() {
   if (true) {
     useEffect(() => {});
   }
 }
 
-// "react-hooks/exhaustive-deps"
+// react-hooks/exhaustive-deps
 function EffectMissingDeps({ name }) {
   useEffect(() => {
     console.log(name);
   }, []);
 }
+
+// react/no-deprecated
+React.render();
+
+// react/no-unescaped-entities
+<div>" hello</div>;
+
+// react/no-unknown-property
+<div class="test" />;
+
+// react/self-closing-comp
+<p></p>;
+
+// react/void-dom-elements-no-children
+<img>test</img>;
+
+// react/jsx-curly-brace-presence
+<p id={"test"} />;
+
+// react/jsx-key
+[1, 2, 3].map(num => <p>{num}</p>);
+
+// react/jsx-no-useless-fragment
+<>
+  <p>test</p>
+</>;
--- index.js
+++ index.js
@@ -37,6 +37,22 @@ module.exports = {
     "react/no-danger-with-children": "warn",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/style-prop-object.md
     "react/style-prop-object": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
+    "react/no-deprecated": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unescaped-entities.md
+    "react/no-unescaped-entities": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+    "react/no-unknown-property": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+    "react/self-closing-comp": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/void-dom-elements-no-children.md
+    "react/void-dom-elements-no-children": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-curly-brace-presence.md
+    "react/jsx-curly-brace-presence": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
+    "react/jsx-key": "warn",
+    //https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-useless-fragment.md
+    "react/jsx-no-useless-fragment": "warn",
     // https://www.npmjs.com/package/eslint-plugin-react-hooks
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
